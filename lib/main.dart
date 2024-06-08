9e2c69b5830b8a741d3d3b7296b7cc2edfef7935
Silva3012 <silvano.n92@gmail.com>
2024-06-08 14:41:19 +0200

    feat: chat screen and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:chatter/screens/auth.dart';
+import 'package:chatter/screens/chat.dart';
+import 'package:chatter/screens/splash.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -23,7 +26,19 @@ class App extends StatelessWidget {
           seedColor: const Color.fromARGB(255, 17, 137, 177),
         ),
       ),
-      home: const AuthScreen(),
+      home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const SplashScreen();
+            }
+
+            if (snapshot.hasData) {
+              return const ChatScreen();
+            }
+
+            return const AuthScreen();
+          }),
     );
   }
 }
