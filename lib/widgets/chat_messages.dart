0b1203a0f5f6504dbd65e9569adc02278eef410e
Silva3012 <silvano.n92@gmail.com>
2024-06-09 13:03:44 +0200

    chore: rename files correctly

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,52 +1,12 @@
 import 'package:flutter/material.dart';
 
-class NewMessage extends StatefulWidget {
-  const NewMessage({super.key});
+class ChatMessages extends StatelessWidget {
+  const ChatMessages({super.key});
 
-  @override
-  State<NewMessage> createState() => _NewMessageState();
-}
-
-class _NewMessageState extends State<NewMessage> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 15, right: 1, bottom: 14),
-      child: Row(
-        children: [
-          Expanded(
-            child: TextField(
-              enableSuggestions: true,
-              controller: _messageController,
-              decoration: const InputDecoration(labelText: 'Message...'),
-            ),
-          ),
-          IconButton(
-            onPressed: _submitMessage,
-            icon: const Icon(Icons.send),
-          ),
-        ],
-      ),
+    return const Center(
+      child: Text('No messages found.'),
     );
-  }
-
-  final _messageController = TextEditingController();
-
-  @override
-  void dispose() {
-    _messageController.dispose();
-    super.dispose();
-  }
-
-  void _submitMessage() {
-    final enteredMessage = _messageController.text;
-
-    if (enteredMessage.trim().isEmpty) {
-      return;
-    }
-
-    //Send to firebase
-
-    _messageController.clear();
   }
 }
